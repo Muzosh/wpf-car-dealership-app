ab3546fa38b14b60720f8e03cbbf8565c2a03fa2
muzos07 <muzosak@seznam.cz>
2019-02-27 00:02:35 +0100

    testing

---
+++ BOOP-Project/BOOP-Project/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BOOP_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoNothingButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Program did nothing!",
+                "Success!",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+        
+        private void InputTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            if (double.TryParse(this.inputTextBox.Text, out double input))
+            {
+                this.resultTextBox.Text = (input * (252 / Math.PI) - 685).ToString();
+            }
+            else if (this.inputTextBox.Text == string.Empty)
+            {
+                this.resultTextBox.Text = string.Empty;
+            }
+            else
+            {
+                this.resultTextBox.Text = "INVALID INPUT";
+            }
+        }
+    }
+}
