95103e7d8ea41f8768335a40d8e78881a0e8bf4f
muzos07 <muzosak@seznam.cz>
2019-03-10 17:12:09 +0100

    work on CarListWindow

---
+++ BOOP-Project/BOOP-Project/Classes/CarList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOP_Project
+{
+    class CarList
+    {
+        public List<Car> Cars { get; private set; }
+    }
+}
---
+++ BOOP-Project/BOOP-Project/Windows/CarListWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BOOP_Project
+{
+    /// <summary>
+    /// Interaction logic for CarListWindow.xaml
+    /// </summary>
+    public partial class CarListWindow : Window
+    {
+        public CarListWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handlers
+        private void AddCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.OpenCarEditWindow(null);
+            this.RefreshGui();
+        }
+
+        private void EditCarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // Helpers
+        private void RefreshGui()
+        {
+
+        }
+
+        // možná nebude potřeba? protože když dataGridu nabindujeme nějaký itemSource, třeba se bude aktualizovat sám
+        private void RefreshCarsDataGrid()
+        {
+
+        }
+
+        private void OpenCarEditWindow(Guid? carID)
+        {
+            CarEditWindow carEditWindow = new CarEditWindow(carID);
+            carEditWindow.ShowDialog();
+        }
+    }
+}
